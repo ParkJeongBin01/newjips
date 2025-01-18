ef5c6fe87f517a9cf0d5073c2a0cf1f9e42ba919
ParkJeongBin <areoin@naver.com>
2025-01-18 23:32:56 +0900

    feat : 아이디 찾기 비밀번호 찾기 각각 버튼 / style수정(활성화, 비활성화)

--- newjips_Frontend/src/pages/certification/Certification.vue
+++ newjips_Frontend/src/pages/certification/Certification.vue
@@ -6,9 +6,22 @@ const find = reactive({
   userId: '',
 });
 
+//상태 변수
+const isFindingId = ref(true);
+const isFindingPassword = ref(false);
+
+const selectOption = (option) => {
+  if (option === 'id') {
+    isFindingId.value = true;
+    isFindingPassword.value = false;
+  } else if (option === 'password') {
+    isFindingId.value = false;
+    isFindingPassword.value = true;
+  }
+};
+
 // 본인인증 창
 const openidentity = ref(true);
-
 const toggleidentity = () => {
   openidentity.value = !openidentity.value;
   openemail.value = false;
@@ -16,7 +29,6 @@ const toggleidentity = () => {
 
 // 이메일 창
 const openemail = ref(false);
-
 const toggleemail = () => {
   openemail.value = !openemail.value;
   openidentity.value = false;
@@ -25,143 +37,146 @@ const toggleemail = () => {
 
 <template>
   <body class="bg-light">
-    <div
-      class="container"
-      style="
-        max-width: 100vw;
-        display: flex;
-        justify-content: center;
-        padding: 5% 0;
-      "
-    >
-      <div
-        class="card"
-        style="width: 35%; max-height: 100%; padding: 20px; height: 50vh"
-      >
-        <h3 class="title">아이디 찾기 / 비밀번호 찾기</h3>
-        <!-- 본인인증 옵션 -->
-        <div
-          class="findidentity"
-          style="
-            border-style: solid;
-            border-width: 1px;
-            border-color: gray;
-            border-radius: 2%;
-          "
-        >
-          <button
-            class="findidentitybutton mb-2 mt-2"
-            type="button"
-            style="
-              padding: 0 3%;
-              border-style: none;
-              background-color: white;
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-              width: 100%;
-            "
-            @click="toggleidentity"
-          >
-            <span style="font-weight: bold"> 본인인증으로 찾기 </span>
-            <span> {{ openidentity ? '∨' : '∧' }} </span>
-          </button>
-          <div v-if="openidentity" class="inidentity mb-2">
-            <div
-              class="identity-phon"
-              style="
-                border-style: solid;
-                border-width: 1px;
-                border-color: gray;
-                border-radius: 5%;
-                max-width: 28%;
-                display: flex;
-                justify-content: center;
-                /* 수평 가운데 정렬 */
-                text-align: center;
-                /* 텍스트 가운데 정렬 */
-                margin: 0 3%;
-                /*좌우 이동*/
-                flex-wrap: wrap;
-              "
+    <div class="container">
+      <div class="card">
+        <div class="selectoption">
+          <button @click="selectOption('id')">아이디 찾기</button>
+          <button @click="selectOption('password')">비밀번호 찾기</button>
+        </div>
+        <div v-if="isFindingId">
+          <!-- 본인인증 옵션 -->
+          <div :class="['findidentity', { inactive: !openidentity }]">
+            <button
+              :class="['findidentitybutton', { inactive: !openidentity }]"
+              type="button"
+              @click="toggleidentity"
             >
-              <img
-                class="mt-2"
-                src="@\assets\images\identity verification.png"
-              />
-              <p>휴대폰 본인인증</p>
+              <span style="font-weight: bold"> 본인인증으로 찾기 </span>
+              <span> {{ openidentity ? '∨' : '∧' }} </span>
+            </button>
+            <div v-if="openidentity" class="inidentity">
+              <div class="identity-phon">
+                <img
+                  class="mt-2"
+                  src="@\assets\images\identity verification.png"
+                />
+                <p>휴대폰 본인인증</p>
+              </div>
+              <p class="text">
+                *본인명의 휴대폰이 아닌 경우, 인증이 불가합니다.
+              </p>
             </div>
-            <p class="mt-2" style="color: gray; margin-left: 2%">
-              *본인명의 휴대폰이 아닌 경우, 인증이 불가합니다.
-            </p>
+          </div>
+
+          <!-- 이메일 옵션 -->
+          <div :class="['findemail', { inactive: !openemail }]">
+            <button
+              :class="['findemailbutton', { inactive: !openemail }]"
+              type="button"
+              @click="toggleemail"
+            >
+              <span style="font-weight: bold"> 이메일로 찾기</span>
+              <span> {{ openemail ? '∨' : '∧' }} </span>
+            </button>
+            <div v-if="openemail" class="inemail">
+              <div class="namewindow">이름</div>
+              <div class="input-container">
+                <label class="nemelabel" for="name"></label>
+                <input
+                  class="name-control"
+                  type="text"
+                  id="name"
+                  v-model="find.username"
+                  placeholder="이름을 입력하세요"
+                />
+              </div>
+              <div class="emaillabel">이메일</div>
+              <div class="input-container">
+                <label class="emaillabellabel" for="email"></label>
+                <input
+                  class="email-control"
+                  type="text"
+                  id="email"
+                  v-model="find.userId"
+                  placeholder="이메일을 입력하세요"
+                />
+              </div>
+              <button class="check" type="sumit">확인</button>
+            </div>
           </div>
         </div>
 
-        <!-- 이메일 옵션 -->
-        <div
-          class="findemail mt-2"
-          style="
-            border-style: solid;
-            border-width: 1px;
-            border-color: gray;
-            border-radius: 2%;
-          "
-        >
-          <button
-            class="findemailbutton mt-2 mb-2"
-            type="button"
-            style="
-              padding: 0 3%;
-              border-style: none;
-              background-color: white;
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-              width: 100%;
-            "
-            @click="toggleemail"
-          >
-            <span style="font-weight: bold"> 이메일로 찾기</span>
-            <span> {{ openemail ? '∨' : '∧' }} </span>
-          </button>
-          <div v-if="openemail" class="inemail">
-            <div class="namewindow mt-3" style="padding: 0 3%">이름</div>
-            <div class="input-container mt-2" style="padding: 0 2%">
-              <label class="nemelabel" for="name"></label>
-              <input
-                class="name-control"
-                type="text"
-                style="box-shadow: none"
-                id="name"
-                v-model="find.username"
-                placeholder="이름을 입력하세요"
-              />
-            </div>
-            <div class="emaillabel mt-3" style="padding: 0 3%">이메일</div>
-            <div class="input-container mt-2 mb-3" style="padding: 0 2%">
-              <label class="emaillabel" for="email"></label>
-              <input
-                class="email-control"
-                type="text"
-                style="box-shadow: none"
-                id="email"
-                v-model="find.userId"
-                placeholder="이메일을 입력하세요"
-              />
-            </div>
+        <div v-if="isFindingPassword">
+          <!-- 본인인증 옵션 -->
+          <div :class="['findidentity', { inactive: !openidentity }]">
             <button
-              class="check mb-3"
-              type="sumit"
-              style="
-                padding: 0 5%;
-                margin: 0 3%;
-                width: 70%;
-                border: 1px solid gray;
-                background-color: gainsboro;
-              "
+              :class="['findidentitybutton', { inactive: !openidentity }]"
+              type="button"
+              @click="toggleidentity"
             >
-              확인
+              <span style="font-weight: bold"> 본인인증으로 찾기 </span>
+              <span> {{ openidentity ? '∨' : '∧' }} </span>
             </button>
+            <div v-if="openidentity" class="inidentity">
+              <div class="id">아이디</div>
+              <div class="input-idcontainer">
+                <label class="idlabel" for="id"></label>
+                <input
+                  class="id-control"
+                  type="text"
+                  id="id"
+                  v-model="find.userId"
+                  placeholder="아이디를 입력하세요."
+                />
+              </div>
+              <div class="identity-phon">
+                <img
+                  class="mt-2"
+                  src="@\assets\images\identity verification.png"
+                />
+                <p>휴대폰 본인인증</p>
+              </div>
+              <p class="text">
+                *본인명의 휴대폰이 아닌 경우, 인증이 불가합니다.
+              </p>
+            </div>
+          </div>
+
+          <!-- 이메일 옵션 -->
+          <div :class="['findemail', { inactive: !openemail }]">
+            <button
+              :class="['findemailbutton', { inactive: !openemail }]"
+              type="button"
+              @click="toggleemail"
+            >
+              <span style="font-weight: bold"> 이메일로 찾기</span>
+              <span> {{ openemail ? '∨' : '∧' }} </span>
+            </button>
+            <div v-if="openemail" class="inemail">
+              <div class="namewindow">이름</div>
+              <div class="input-container">
+                <label class="nemelabel" for="name"></label>
+                <input
+                  class="name-control"
+                  type="text"
+                  id="name"
+                  v-model="find.username"
+                  placeholder="이름을 입력하세요"
+                />
+              </div>
+              <div class="emaillabel">이메일</div>
+              <div class="input-container">
+                <label class="emaillabellabel" for="email"></label>
+                <input
+                  class="email-control"
+                  type="text"
+                  id="email"
+                  v-model="find.userId"
+                  placeholder="이메일을 입력하세요"
+                />
+              </div>
+              <button class="check" type="submit">확인</button>
+            </div>
           </div>
         </div>
       </div>
@@ -170,24 +185,120 @@ const toggleemail = () => {
 </template>
 
 <style scope>
-/* .container {
+.container {
   max-width: 100vw;
-
   display: flex;
   justify-content: center;
   padding: 5% 0;
-} */
-/* .card {
+}
+.card {
   width: 35%;
   max-height: 100%;
   padding: 20px;
-  height: 50vh;
-} */
-.title {
+  height: 55vh;
+}
+.selectoption {
   display: flex;
   justify-content: center;
+  font-size: x-large;
+  margin-bottom: 2%;
+}
+.selectoption button {
+  border-style: none;
+  background-color: white;
+}
+/* 본인인증 옵션 */
+.findidentity {
+  border: 1px solid gray;
+  border-radius: 2%;
+}
+.findidentitybutton {
+  padding: 0 3%;
+  border-style: none;
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-top: 2%;
+  margin-bottom: 2%;
+}
+.inidentity {
+  margin-bottom: 2%;
+}
+.id {
+  padding: 0 2%;
+  margin-bottom: 1%;
+}
+.input-idcontainer {
+  padding: 0 2%;
+}
+.id-control {
+  padding: 2%;
+  border-radius: 3%;
+  margin-bottom: 2%;
 }
 .identity-phon {
+  border: 1px solid gray;
+  border-radius: 5%;
   width: 20%;
+  max-width: 28%;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  margin: 0 3%;
+  flex-wrap: wrap;
+}
+.text {
+  margin-top: 2%;
+  color: gray;
+  margin-left: 2%;
+}
+/* 이메일 옵션 */
+.findemail {
+  border: 1px solid gray;
+  border-radius: 2%;
+  margin-top: 2%;
+}
+.findemailbutton {
+  padding: 0 3%;
+  border-style: none;
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-top: 2%;
+  margin-bottom: 2%;
+}
+.namewindow {
+  margin-top: 3%;
+  padding: 0 3%;
+}
+.input-container {
+  margin-top: 2%;
+  padding: 0 2%;
+}
+.name-control {
+  margin-top: -1%;
+  box-shadow: none;
+}
+.emaillabel {
+  margin-top: 3%;
+  padding: 0 3%;
+}
+.email-control {
+  margin-top: -1%;
+  box-shadow: none;
+}
+.check {
+  margin: 3% 0 3% 2%; /* 상 우 하 좌 */
+  padding: 0 5%;
+  width: 90%;
+  border: 1px solid gray;
+  background-color: gainsboro;
+}
+.inactive {
+  background-color: lightgray;
 }
 </style>

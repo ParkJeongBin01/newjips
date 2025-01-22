dd4fe1af37aed905df86fd15ac08e9441226a6db
ParkJeongBin <areoin@naver.com>
2025-01-22 10:45:57 +0900

    feat:login page에서 '아이디 찾기','비밀번호 찾기'를 누르면 해당 메뉴가 보이도록 설정

--- newjips_Frontend/src/router/certification.js
+++ newjips_Frontend/src/router/certification.js
@@ -1,6 +1,6 @@
 export default [
   {
-    path: '/auth//login/certification',
+    path: '/auth/login/certification',
     name: 'certification',
     component: () => import('@/pages/certification/Certification.vue'),
   },
